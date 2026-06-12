f64aa98cd5e38120af645813dabae61cbe01f4dd
Yaron Efrat <yaronefrat92@gmail.com>
2026-06-12 15:30:37 +0200

    #4: Add application status query and use case
    Added `GetApplicationStatusQuery` and `ApplicationStatus` records to represent the query and status. Implemented `GetApplicationStatusUseCase` to handle the query and return the status. Updated the solution to include the new `SecondBrain.Application` project and its test project. Added tests for the use case to verify functionality and null handling.

---
+++ tests/SecondBrain.Application.Tests/UseCases/GetApplicationStatusUseCaseTests.cs
@@ -0,0 +1,31 @@
+using SecondBrain.Application.Queries;
+using SecondBrain.Application.UseCases;
+using NUnit.Framework;
+
+namespace SecondBrain.Application.Tests.UseCases;
+
+[TestFixture]
+public sealed class GetApplicationStatusUseCaseTests
+{
+    [Test]
+    public void Handle_ReturnsReadyStatus()
+    {
+        var useCase = new GetApplicationStatusUseCase();
+
+        var status = useCase.Handle(new GetApplicationStatusQuery());
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(status.Name, Is.EqualTo("SecondBrain"));
+            Assert.That(status.IsReady, Is.True);
+        });
+    }
+
+    [Test]
+    public void Handle_ThrowsForNullQuery()
+    {
+        var useCase = new GetApplicationStatusUseCase();
+
+        Assert.Throws<ArgumentNullException>(() => useCase.Handle(null!));
+    }
+}
