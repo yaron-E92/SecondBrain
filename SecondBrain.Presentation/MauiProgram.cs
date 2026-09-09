143fe8108cc13d55888eefdecb3a8394427e8ff7
Yaron Efrat <yaronefrat92@gmail.com>
2026-09-09 12:37:53 +0200

    feat(ux): register inbox processing flow

--- SecondBrain.Presentation/MauiProgram.cs
+++ SecondBrain.Presentation/MauiProgram.cs
@@ -26,6 +26,7 @@ public static class MauiProgram
         builder.Services.AddSingleton<INotionExportSourcePicker, MauiNotionExportSourcePicker>();
         builder.Services.AddSingleton<InboxViewModel>();
         builder.Services.AddSingleton<DashboardViewModel>();
+        builder.Services.AddSingleton<InboxProcessViewModel>();
         builder.Services.AddSingleton<ParaBrowserViewModel>();
         builder.Services.AddSingleton<CoreSearchViewModel>();
         builder.Services.AddSingleton<CoreEditorViewModel>();
@@ -35,6 +36,7 @@ public static class MauiProgram
         builder.Services.AddSingleton<AppShell>();
         builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<InboxPage>();
+        builder.Services.AddSingleton<InboxProcessPage>();
         builder.Services.AddSingleton<ParaBrowserPage>();
         builder.Services.AddSingleton<CoreSearchPage>();
         builder.Services.AddSingleton<CoreEditorPage>();
