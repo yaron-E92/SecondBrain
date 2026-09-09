409f9959f2603c74a4fa4c957f00c16e9f0929e7
Yaron Efrat <yaronefrat92@gmail.com>
2026-09-09 14:22:32 +0200

    test(ux): lock separate create and edit surfaces

--- tests/SecondBrain.Architecture.Tests/PresentationNavigationContractTests.cs
+++ tests/SecondBrain.Architecture.Tests/PresentationNavigationContractTests.cs
@@ -8,10 +8,7 @@ public sealed class PresentationNavigationContractTests
     [Test]
     public void AppShell_PrimaryNavigationExpressesProductLifecycle()
     {
-        var source = File.ReadAllText(Path.Combine(
-            FindRepositoryRoot(),
-            "SecondBrain.Presentation",
-            "AppShell.cs"));
+        var source = ReadPresentationFile("AppShell.cs");
 
         Assert.Multiple(() =>
         {
@@ -32,29 +29,67 @@ public sealed class PresentationNavigationContractTests
     [Test]
     public void AppShell_ImplementationAndUtilitySurfacesRemainContextual()
     {
-        var source = File.ReadAllText(Path.Combine(
-            FindRepositoryRoot(),
-            "SecondBrain.Presentation",
-            "AppShell.cs"));
+        var source = ReadPresentationFile("AppShell.cs");
 
         Assert.Multiple(() =>
         {
             Assert.That(source, Does.Contain("HiddenItem(\"inbox-process\""));
             Assert.That(source, Does.Contain("HiddenItem(\"journals\""));
+            Assert.That(source, Does.Contain("HiddenItem(\"create\""));
+            Assert.That(source, Does.Contain("HiddenItem(\"editor\""));
             Assert.That(source, Does.Contain("HiddenItem(\"settings\""));
             Assert.That(source, Does.Contain("HiddenItem(\"data-import\""));
-            Assert.That(source, Does.Contain("HiddenItem(\"editor\""));
 
-            Assert.That(source, Does.Not.Contain("PrimaryItem(\"journals\""));
-            Assert.That(source, Does.Not.Contain("PrimaryItem(\"settings\""));
-            Assert.That(source, Does.Not.Contain("PrimaryItem(\"data-import\""));
-            Assert.That(source, Does.Not.Contain("PrimaryItem(\"editor\""));
-            Assert.That(source, Does.Not.Contain("PrimaryContent(\"journals\""));
-            Assert.That(source, Does.Not.Contain("PrimaryContent(\"settings\""));
-            Assert.That(source, Does.Not.Contain("PrimaryContent(\"data-import\""));
-            Assert.That(source, Does.Not.Contain("PrimaryContent(\"editor\""));
+            foreach (var route in new[] { "journals", "create", "editor", "settings", "data-import" })
+            {
+                Assert.That(source, Does.Not.Contain($"PrimaryItem(\"{route}\""));
+                Assert.That(source, Does.Not.Contain($"PrimaryContent(\"{route}\""));
+            }
         });
     }
+
+    [Test]
+    public void KnowledgeCreationAndEditing_AreSeparateContextualSurfaces()
+    {
+        var shell = ReadPresentationFile("AppShell.cs");
+        var create = ReadPresentationFile("CoreCreatePage.cs");
+        var edit = ReadPresentationFile("CoreEditPage.cs");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(shell, Does.Contain("CoreCreatePage createPage"));
+            Assert.That(shell, Does.Contain("CoreEditPage editPage"));
+            Assert.That(shell, Does.Not.Contain("CoreEditorPage coreEditorPage"));
+
+            Assert.That(create, Does.Contain("Title = \"Create knowledge\""));
+            Assert.That(create, Does.Contain("//editor"),
+                "Create may return to a source item after derivation, but it remains its own route.");
+            Assert.That(create, Does.Not.Contain("Existing item"));
+
+            Assert.That(edit, Does.Contain("Title = \"Edit knowledge\""));
+            Assert.That(edit, Does.Contain("Creation is a separate flow."));
+            Assert.That(edit, Does.Not.Contain("New item"));
+        });
+    }
+
+    [Test]
+    public void DesktopRail_UsesExplicitReadableInactiveText()
+    {
+        var source = ReadPresentationFile("AppShell.cs");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(source, Does.Contain("NavigationText = Color.FromArgb(\"#D9E4EE\")"));
+            Assert.That(source, Does.Contain("Shell.SetUnselectedColor(this, NavigationText)"));
+            Assert.That(source, Does.Contain("Shell.SetUnselectedColor(item, NavigationText)"));
+        });
+    }
+
+    private static string ReadPresentationFile(string fileName) =>
+        File.ReadAllText(Path.Combine(
+            FindRepositoryRoot(),
+            "SecondBrain.Presentation",
+            fileName));
 
     private static string FindRepositoryRoot()
     {
